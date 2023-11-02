fedb5b466777e24b5c8d7278075a45ed8b6ac76a
pitust <piotr@stelmaszek.com>
2023-11-02 15:02:17 +0000

    fix(romangle): hopefully this is the last time

--- romangle.py
+++ romangle.py
@@ -174,7 +174,6 @@ def cmd_mangle(*, i, o = 'roms'):
 					svg = svg.replace('width="375" height="635" viewBox="0 0 375 635"', 'width="750" height="1270" viewBox="0 0 375 635"')
 					wr.write(ROM8Tag.faceSVG, svg.encode())
 
-					print('romangle v2 doing face')
 					x, y = 80, 89
 					w, h = 192, 63
 					scale = 3
