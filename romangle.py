6d82fa684b0526814fef7a5ce525aacb47ae2c47
pitust <piotr@stelmaszek.com>
2023-11-02 16:47:37 +0000

    fix(romangle): make the ugly svg hacks do the correct thing

--- romangle.py
+++ romangle.py
@@ -174,7 +174,7 @@ def cmd_mangle(*, i, o = 'roms'):
 				if 'faceSVG' in props:
 					svg = props['faceSVG'].decode()
 					if 'width=' not in svg[:1000]:
-						svg = svg.replace('<svg version=', '<svg width="375" height="635" version=')
+						svg = svg.replace('viewBox="', 'width="375" height="635" viewBox="')
 					svg = svg.replace('width="376" height="635" viewBox="0 0 376 635"', 'width="375" height="635" viewBox="0 0 375 635"')
 					if 'width="375" height="635" viewBox="0 0 375 635"' in svg:
 						svg = svg.replace('width="375" height="635" viewBox="0 0 375 635"', 'width="750" height="1270" viewBox="0 0 375 635"')
